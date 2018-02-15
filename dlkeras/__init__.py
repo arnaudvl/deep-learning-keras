3824a272ad5463fab5986d206f4b20cfea35f514
arnaudvl <arnaudvlooveren@gmail.com>
2018-02-15 03:29:44 +0000

    Update __init__.py

--- dlkeras/__init__.py
+++ dlkeras/__init__.py
@@ -1,1 +1,26 @@
-
+from datetime import datetime
+from keras import backend as K
+from keras.applications.vgg16 import VGG16
+from keras.applications.vgg19 import VGG19
+from keras.applications.resnet50 import ResNet50
+from keras.applications.xception import Xception
+from keras.applications.inception_v3 import InceptionV3
+from keras.applications.inception_resnet_v2 import InceptionResNetV2
+from keras.applications.mobilenet import MobileNet
+from keras.callbacks import EarlyStopping, ModelCheckpoint, Callback, ReduceLROnPlateau
+from keras.layers import concatenate, Dense, Dropout, Flatten, Activation, GlobalAveragePooling2D, GlobalMaxPooling2D, Input
+from keras.layers import Conv2D, AveragePooling2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.losses import mean_squared_error,mean_absolute_error,categorical_crossentropy,binary_crossentropy
+from keras.models import Model, load_model, Sequential
+from keras.optimizers import Adam, SGD, RMSprop, Adagrad, Adadelta, Adamax, Nadam
+from keras.preprocessing.image import ImageDataGenerator
+from keras.regularizers import l1
+from keras.utils import to_categorical
+from matplotlib import pyplot as plt
+import numpy as np
+import os
+import pandas as pd
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import StratifiedKFold, train_test_split
+from sklearn.preprocessing import Imputer, MinMaxScaler, StandardScaler
