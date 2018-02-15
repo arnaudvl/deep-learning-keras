d61f82e712cd32e326ce29dbcc938a0bcb83508c
arnaudvl <arnaudvlooveren@gmail.com>
2018-02-15 03:46:02 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,13 @@
 
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='dlkeras',
+      version='1.0.1',
+      description='Create, train and make predictions with CNN and feedforward NN models using keras.',
+      author='Arnaud Van Looveren',
+      author_email='arnaudvlooveren@gmail.com',
+      url='https://github.com/arnaudvl/deep-learning-keras',
+      license='MIT',
+      install_requires=['keras','numpy','pandas','scikit-learn','scipy'],
+      packages=find_packages())
